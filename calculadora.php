0668fb82068386000c026d07d7ea85a68006be10
VinnyS2 <vinicius-almeida01@hotmail.com>
2020-03-11 16:22:31 -0300

    Criacao da calculadora em php

---
+++ calculadora.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+    <html lang = "pt-BR">
+    <head>
+        <meta charset="utf-8"/>
+         <title> PROCESSA OS DADU IN THE PEAGAPE </title>
+    </head>
+    <body>
+        <?php
+
+          $nmr1 = $_POST["numero1"];
+          $nmr2 = $_POST["numero2"];
+          $op = $_POST["op"];
+          $resultado = 0;
+
+          if($op == "soma")
+          {
+             $resultado = $nmr1 + $nmr2;
+            echo '<h3>A soma deu '.$resultado.'</h3>';
+          }
+          if($op == "sub")
+          {
+            $resultado = $nmr1 - $nmr2;
+            echo '<h3>A subtração deu '.$resultado.'</h3>';
+          }
+          if($op == "div")
+          {
+            $resultado = $nmr1 / $nmr2;
+            echo '<h3>A divisão deu '.$resultado.'</h3>';
+          }
+          if($op == "mult")
+          {
+            $resultado = $nmr1 * $nmr2;
+            echo '<h3>A multiplicação deu '.$resultado.'</h3>';
+          }
+
+
+        ?>
+    </body>
+</html>
